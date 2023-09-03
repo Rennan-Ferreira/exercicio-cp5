926caaba00aabd36b0a9e8094e4dfa38d51d7397
Rennan-Ferreira <rm99364@fiap.com.br>
2023-09-03 13:41:18 -0300

    configurando as rotas

--- exercicio-cp5/src/main.jsx
+++ exercicio-cp5/src/main.jsx
@@ -3,3 +3,26 @@ import Home from './routes/Home/index.jsx'
 import Produtos from './routes/Produtos/index.jsx'
 import Error from './routes/Error/index.jsx'
 import EditarProduto from './routes/EditarProdutos/index.jsx'
+import { Children } from 'react'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children:[
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto/>
+      }
+    ]
+  }
+])
