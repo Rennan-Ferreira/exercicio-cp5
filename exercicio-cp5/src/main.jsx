af08f1f6b8386e369ed260943661e854e60a89e1
Rennan-Ferreira <rm99364@fiap.com.br>
2023-09-03 13:49:07 -0300

    rotas configuradas com sucesso.

--- exercicio-cp5/src/main.jsx
+++ exercicio-cp5/src/main.jsx
@@ -26,3 +26,9 @@ const router = createBrowserRouter([
     ]
   }
 ])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
